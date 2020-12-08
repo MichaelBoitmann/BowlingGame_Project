5b3aa943bd83c43a5ef4f7012aaefc8f8f4eabf3
Michael Boitmann <90054169@learn.cpp.ac.nz>
2020-12-08 17:00:39 +1300

    changed the syntax from "thow" to "throws"

--- BowlingGame.py
+++ BowlingGame.py
@@ -6,22 +6,22 @@
 
 class BowlingGame(object):
     def __init__(self):
-        self.throw= []
+        self.throws= []
         self.score= 0
 
-    def throw(self,pins):
-        self.throw.append(pins)
+    def throw(self, pins):
+        self.throws.append(pins)
 
 
-    def calculate_score(self):
+    def calculate_score(self, frames):
         ball = 0
         for frames in range(10):
             if self.throws[ball]==10:
                 self.score +=10 + self.throws[ball+1] + self.throws[ball +2]
                 ball += 1
-            elif self.throw[ball] + self.throws[ball+1] == 10:
-                self.score += 10 + self.throws [ball +2]
+            elif self.throws[ball] + self.throws[ball+1] == 10:
+                self.score += 10 + self.throws[ball +2]
                 ball +=2
             else:
-                self.score += self.throws[ball] + self.throw[ball + 1]
+                self.score += self.throws[ball] + self.throws[ball + 1]
                 ball += 2
